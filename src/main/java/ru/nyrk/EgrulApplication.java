603f6a8d7704fcbae75c09a0742b7ee804cf10d2
Nyrkov Oleg <sbt-nyrkov-oa@sberbank.ru>
2017-01-27 15:07:07 +0300

    Созда проект

---
+++ src/main/java/ru/nyrk/EgrulApplication.java
@@ -0,0 +1,12 @@
+package ru.nyrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgrulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgrulApplication.class, args);
+	}
+}
