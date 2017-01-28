df7707ea1c608c294a85dd9ff2a9cfd06011331b
Nyrkov Oleg <sbt-nyrkov-oa@sberbank.ru>
2017-01-28 13:13:58 +0300

    адапторы

---
+++ generate/src/main/java/ru/nyrk/egrul/generate/DateAdapter.java
@@ -0,0 +1,29 @@
+package ru.nyrk.egrul.generate;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * Адаптер для JAXB, переводит xml представление xs:date в java.util.Date и обратно. Формат в xml CCYY-MM-DD
+ */
+public class DateAdapter {
+
+    private static final FastDateFormat DATE_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd");
+
+    public static Date parseDate(String dateFromXmlTag) {
+        try {
+            return DATE_FORMAT.parse(dateFromXmlTag);
+        } catch (ParseException e) {
+            throw new RuntimeException("Не удалось распарсить дату из xml документа");
+        }
+    }
+
+    public static String printDate(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return DATE_FORMAT.format(date);
+    }
+}
